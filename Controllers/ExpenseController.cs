dca16ac56b875d0d6ec54f92918a906196e5577a
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-13 16:10:42 -0700

    Added expenses logic

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,41 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View(objList);
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Borrower { get; set; }
+        public string Lender { get; set; }
+        [System.ComponentModel.DisplayName("Item name")]
+        public string ItemName { get; set; }
+    }
+}
